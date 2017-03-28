dc36ba8ac76908334f98587586f5600e2563ace6
liuchang0812 <liuchang0812@gmail.com>
2017-03-28 13:15:49 +0800

    avoid incomplete download

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info[0] != 2 or sys.version_info[1] < 6:
     sys.exit('Sorry, only python 2.6 or 2.7 is supported')
 
 setup(name='qcloud_cos_v4',
-      version='0.0.13',
+      version='0.0.14',
       description='python sdk for tencent qcloud cos v4.0',
       long_description=open('README.rst', 'r').read(),
       license='MIT License',
